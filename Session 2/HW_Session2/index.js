e5d6c6bcfaa7666771e025cd3c028d963043c9bb
Viet Do <viet.doxv@gmail.com>
2020-12-18 10:40:12 +0700

    task 3

---
+++ Session 2/HW_Session2/index.js
@@ -0,0 +1,35 @@
+//________________3_____________
+//a
+alert("3a");
+for(let i = 0; i < 7; ++i)
+    console.log(i);
+//b
+alert("3b");
+let n = prompt("Enter a number: ")
+for(let i = 0; i < n; ++i)
+    console.log(i);
+//c
+alert("3c");
+let n = prompt("Enter n");
+for(let i = 3; i < n; ++i)
+    console.log(i)
+//d
+alert("3d");
+let n = prompt("Enter n");
+let c = prompt("Enter c");
+for(; c < n; c++)
+    console.log(c);
+//e
+alert("3e");
+let n = prompt("Enter n");
+let c = prompt("Enter c");
+for(; c < n; c += 3)
+    console.log(c);
+//f
+alert("3f");
+let n = prompt("Enter n");
+let c = prompt("Enter c");
+let s = prompt("Enter s");
+for(; c < n; c += s)
+    console.log(c);
+//___________________4_______________
