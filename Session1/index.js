c5756a86075846bceff6e161107fd866a9e5ee72
Viet Do <viet.doxv@gmail.com>
2020-12-18 10:22:42 +0700

    hw_session1

---
+++ Session1/index.js
@@ -0,0 +1,37 @@
+//______________3______________
+//a
+message = "Coding is great"; 
+console.log(message);
+//b
+studentCount = 0;
+console.log(studentCount);
+//______________4______________
+//a
+message = "Coding might not be easy, but still great";
+console.log(message);
+//b
+studentCount = 16;
+console.log(studentCount);
+//c
+console.log(message.toLowerCase());
+//d
+console.log(++studentCount);
+//_______________5______________
+alert("You look beautiful today");
+//_______________6______________
+_name = prompt("Hi there, your name please?");
+alert('Hi '+ _name);
+//_______________7______________
+firstName = prompt("Enter your first name");
+lastName = prompt("Enter your last name");
+alert("Hi " + lastName +' ' + firstName);
+//_______________8______________
+lenOfSquare = prompt("Enter side length of the square");
+alert(`The square area is ${lenOfSquare*lenOfSquare}`);
+//_______________9______________
+R = prompt("Enter the radius of the circle");
+alert(`The circle area is ${3.14*R*R}`);
+//_______________10_____________
+CelciusTemp = prompt("Enter the temperature in Celcius");
+alert(`${CelciusTemp} (C) = ${CelciusTemp*1.8+32} (F)`);
+//_____________THE_END___________________________
