605c39a3643761b4009ce8e16152a44e98ec4616
Viet Do <viet.doxv@gmail.com>
2020-12-19 01:12:34 +0700

    3_4_5

--- Session 2/index.js
+++ Session 2/index.js
@@ -1,35 +1,47 @@
-//________________3_____________
-//a
-alert("3a");
-for(let i = 0; i < 7; ++i)
-    console.log(i);
-//b
-alert("3b");
-let n = prompt("Enter a number: ")
-for(let i = 0; i < n; ++i)
-    console.log(i);
-//c
-alert("3c");
-let n = prompt("Enter n");
-for(let i = 3; i < n; ++i)
-    console.log(i)
-//d
-alert("3d");
-let n = prompt("Enter n");
-let c = prompt("Enter c");
-for(; c < n; c++)
-    console.log(c);
-//e
-alert("3e");
-let n = prompt("Enter n");
-let c = prompt("Enter c");
-for(; c < n; c += 3)
-    console.log(c);
-//f
-alert("3f");
-let n = prompt("Enter n");
-let c = prompt("Enter c");
-let s = prompt("Enter s");
-for(; c < n; c += s)
-    console.log(c);
-//___________________4_______________
+// //________________3_____________
+// console.log("_________________3a______________");
+// for(let i = 0; i < 7; ++i)
+//     console.log(i);
+
+// console.log("_________________3b______________");
+// let n = Number(prompt("3b. Enter a number: "));
+// for(let i = 0; i < n; ++i)
+//     console.log(i);
+
+// console.log("_________________3c______________");
+// n = Number(prompt("3c. Enter n"));
+// for(let i = 3; i < n; ++i)
+//     console.log(i)
+
+// console.log("_________________3d______________");
+// n = Number(prompt("3d. Enter n"));
+// let c = Number(prompt("3d. Enter c"));
+// for(; c < n; c++)
+//     console.log(c);
+
+// console.log("_________________3e______________");
+// n = Number(prompt("3e. Enter n"));
+// c = Number(prompt("3e. Enter c"));
+// for(; c < n; c += 3)
+//     console.log(c);
+
+// console.log("_________________3f______________");
+// n = Number(prompt("3f. Enter n"));
+// c = Number(prompt("3f. Enter c"));
+// let s = Number(prompt("3f. Enter s"));
+// for(; c < n; c += s)
+//     console.log(c);
+
+// //___________________4_______________
+// let factOfN = 1
+// n = Number(prompt("Enter a number"));
+// for(let i = 2; i < n + 1; ++i)
+//     factOfN *= i;
+// alert(`The factorial of ${n} is ${factOfN}`);
+//___________________5_______________
+let TheirAge = ~~prompt("How old are you?");
+if(TheirAge < 14)
+    alert("You are not old enough to view this content");
+else
+    alert("Enjoy!");
+//___________________6_______________
