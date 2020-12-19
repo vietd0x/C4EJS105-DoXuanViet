75a768bb8a89c972976770abf146be765d4656e4
Viet Do <viet.doxv@gmail.com>
2020-12-19 12:46:40 +0700

    6_7_8_9

--- Session 2/index.js
+++ Session 2/index.js
@@ -34,14 +34,62 @@
 
 // //___________________4_______________
 // let factOfN = 1
-// n = Number(prompt("Enter a number"));
+// n = Number(prompt("4. Enter a number"));
 // for(let i = 2; i < n + 1; ++i)
 //     factOfN *= i;
 // alert(`The factorial of ${n} is ${factOfN}`);
-//___________________5_______________
-let TheirAge = ~~prompt("How old are you?");
-if(TheirAge < 14)
-    alert("You are not old enough to view this content");
-else
-    alert("Enjoy!");
-//___________________6_______________
+// //___________________5_______________
+// let TheirAge = ~~prompt("5. How old are you?");
+// if(TheirAge < 14)
+//     alert("You are not old enough to view this content");
+// else
+//     alert("Enjoy!");
+// //___________________6_______________
+// let x = Number(prompt("6. Enter a number"));
+// console.log(x);
+// if(x < 4.5)
+//     alert("Lower half of 9");
+// else
+//     alert("Higher half of 9");
+
+// //___________________7_______________
+// n = Number(prompt("7. n = "));
+// x = Number(prompt("7. x = "));
+// if(x < n/2)
+//     alert(x + " is in lower half of " + n);
+// else
+//     alert(x + " is in higher half of " + n);
+// //___________________8_______________
+// x = Number(prompt("8. x ="));
+// if(x == 0)
+//     alert("x = 0");
+// else if(x % 2 == 1)
+//     alert(x + " is an odd number");
+// else
+//     alert(x + " is an even number");
+// //___________________9_______________
+// console.log("________________9a________________");
+// for(let i = 0; i < 3; ++i)
+//     console.log('L');
+// for(let j = 0; j < 3; ++j)
+//     console.log('H');
+// console.log("________________9b________________");
+// n = Number(prompt("Enter the total number of L's and H's?"));
+// let l = Math.ceil(n/2);
+// for(let i = 0; i < l; ++i)
+//     console.log('L');
+// for(let j = 0; j < n - l; ++j)
+//     console.log('H');
+// console.log("________________9c________________");
+// for(let i = 0; i <8; i++)
+//     if(i%2 == 0)
+//         console.log(0);
+//     else
+//         console.log(1);
+console.log("________________9d________________");
+n = ~~prompt("9d. Enter the total number of 1's and 0's?");
+for(let i = 0; i < n; i++)
+    if(i % 2 == 0)
+        console.log(0);
+    else
+        console.log(1);
