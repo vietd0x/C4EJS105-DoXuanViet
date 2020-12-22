5dadead6afb94f5f65d08db4ac3ec62bdc0af271
Viet Do <viet.doxv@gmail.com>
2020-12-22 17:36:13 +0700

    update

--- Session3_Array/Hwork/Ex3_8.js
+++ Session3_Array/Hwork/Ex3_8.js
@@ -1,48 +1,76 @@
-console.log('_______2________');
-const s = "Hello beauty there";
-let str = s.split(" ");
-console.log(str);
+// console.log('_______2________');
+// const s = "Hello beauty there";
+// let words = s.split(" ");
+// console.log(words);
 
-console.log('_______3________');
-const a = [4, 1, 2001, 'viet'];
-console.log(...a);
+// console.log('_______3________');
+// const a = [4, 1, 2001, 'viet'];
+// console.log(...a);
 
-console.log('_______4________');
-let taskRequest;
-let clothes = ['Jeans', "T-shirt", "Socks"];
-while(true)
-{
-    taskRequest = prompt("Hi there, welcome to shop admin panel, what do u want(C, R, U, D)?");
-    if(taskRequest != 'c' && taskRequest != 'r' && taskRequest != 'u' && taskRequest != 'd')
-        alert("This command is not supported");
-    else
-    {
-        let n = clothes.length;
-        if(taskRequest == 'r'){
-            for(let i = 0; i < n; ++i)
-               alert(`${i+1}. ${clothes[i]}`);
-        }
-        else if(taskRequest == 'c')
-        {
-            let NewItem = prompt("Enter the name of the new item");
-            clothes.push(NewItem);
-        }
-        else if(taskRequest == 'u')
-        {
-            let index_ = Number(prompt("Enter the position u want to update"));
-            let itemUpdate = prompt("Enter the new name");
-            clothes[index_ - 1] = itemUpdate;
-        }
-        else 
-        {
-            let index_ = Number(prompt("Enter the position u want to delete"));
-            clothes.splice(index_ - 1, 1);
-        }
-        alert("Done");
-        let exit = prompt("press x to exit")
-        if(exit == 'x')
-            break;
-    }
-}
-
- 
+// console.log('_______4________');
+// let taskRequest;
+// let clothes = ['Jeans', "T-shirt", "Socks"];
+// while(true)
+// {
+//     taskRequest = prompt("Hi there, welcome to shop admin panel, what do u want(C, R, U, D)?");
+//     if(taskRequest != 'c' && taskRequest != 'r' && taskRequest != 'u' && taskRequest != 'd')
+//         alert("This command is not supported");
+//     else
+//     {
+//         let n = clothes.length;
+//         if(taskRequest == 'r'){
+//             for(let i = 0; i < n; ++i)
+//                alert(`${i+1}. ${clothes[i]}`);
+//         }
+//         else if(taskRequest == 'c')
+//         {
+//             let NewItem = prompt("Enter the name of the new item");
+//             clothes.push(NewItem);
+//         }
+//         else if(taskRequest == 'u')
+//         {
+//             let index_ = Number(prompt("Enter the position u want to update"));
+//             let itemUpdate = prompt("Enter the new name");
+//             clothes[index_ - 1] = itemUpdate;
+//         }
+//         else 
+//         {
+//             let index_ = Number(prompt("Enter the position u want to delete"));
+//             clothes.splice(index_ - 1, 1);
+//         }
+//         alert("Done");
+//         let exit = prompt("press x to exit")
+//         if(exit == 'x')
+//             break;
+//     }
+// }
+// //___________________________5___________________
+// let SeqOfNum = prompt('Enter a squence of Number, separated by commas(,)');
+// let NumList = SeqOfNum.split(',');
+// let n = NumList.length;
+// let sum = 0;
+// for(let i = 0; i < n; ++i)
+//     sum += Number(NumList[i]);
+// alert("the sum of them is " + sum);
+// //___________________________6____________________
+// SeqOfNum = prompt('Enter a squence of Number, separated by commas(,)');
+// NumList = SeqOfNum.split(',');
+// NumList.sort();
+// alert(`The smallest number is ${NumList[0]}`);
+// //___________________________7____________________
+// const arr = [3, 4, 6, -9, 10, -88, 2];
+// let x = Number(prompt("Enter a number"));
+// n = arr.length;
+// for(let i = 0; i < n; i++)
+//     if(x == arr[i])
+//         alert(x + " is FOUND in my array at index " + i);
+// alert(x + " is NOT found in my array");
+//___________________________8____________________
+let sheepSize = [5, 7, 300, 90, 24, 50, 75];
+console.log('_______8.1________');
+console.log('Hello, my name is Viet and here is my sheep sizes:');
+console.log(...sheepSize);
+console.log('_______8.2________');
+console.log(`Now my biggest sheep has size ${Math.max(...sheepSize)}, let's shave it`);
+console.log('_______8.3________');
+console.log("After shearing, here is my flock");
--- Session3_Array/checkPoint/index.js
+++ Session3_Array/checkPoint/index.js
@@ -1,25 +1,25 @@
-// /*
-// Write a script asking users their username, if the entered username is too long (>15 characters), 
-// tell them that and ask them to re-enter a new username, if the username is short enough (<=15 characters), tell them it is good
-// */
-// let username;
-// let len;
-// do{
-//     username = prompt("register an username");
-//     len = username.length;
-// }while(len > 15)
-// alert("Good username");
+/*
+Write a script asking users their username, if the entered username is too long (>15 characters), 
+tell them that and ask them to re-enter a new username, if the username is short enough (<=15 characters), tell them it is good
+*/
+let username;
+let len;
+do{
+    username = prompt("register an username");
+    len = username.length;
+}while(len > 15)
+alert("Good username");
 /*__________________________________________________________
 fun quizz
 */
-// let ans
-// do{
-//     ans = prompt("How many legs does a spider have?\n1. None\n2. 4 legs\n3. 8 legs\n4. 12 legs");
-// }while(ans != 1 && ans != 2 && ans != 4 && ans != 3)
-// if(ans == 3)
-//     alert("Bravo, you are correct");
-// else
-//     alert("Good luck next time");
+let ans
+do{
+    ans = prompt("How many legs does a spider have?\n1. None\n2. 4 legs\n3. 8 legs\n4. 12 legs");
+}while(ans != 1 && ans != 2 && ans != 4 && ans != 3)
+if(ans == 3)
+    alert("Bravo, you are correct");
+else
+    alert("Good luck next time");
 
 /*___________________________________________________________
 Ng dung nhap n so nguyen ngau nhien tu ban fim
@@ -29,37 +29,40 @@ c. Loai bo cac so trung nhau o 2 day
 */
 let n = Number(prompt("Nhap n:"));
 let arr = [];
-for(let i = 0; i < n; ++i)
-{
+for (let i = 0; i < n; ++i) {
     arr[i] = Number(prompt(`Nhap so nguyen thu ${i + 1}`));
 }
-// arr.sort();
-// alert(`day so nguyen sau khi sap xep: ${arr}`);
+arr.sort();
+alert(`day so nguyen sau khi sap xep: ${arr}`);
 
-// let evenArr = [];
-// let oddArr = [];
-// for(let i = 0; i < n; ++i)
-// {
-//     if(arr[i] % 2 == 1)
-//         oddArr.push(arr[i]);
-//     else if(arr[i] == 0)
-//         continue;
-//     else
-//         evenArr.push(arr[i]);
-// }
-// alert(`day chan: ${evenArr}\nday le: ${oddArr}`);
+let evenArr = [];
+let oddArr = [];
+for(let i = 0; i < n; ++i)
+{
+    if(arr[i] % 2 == 1)
+        oddArr.push(arr[i]);
+    else if(arr[i] == 0)
+        continue;
+    else
+        evenArr.push(arr[i]);
+}
+alert(`day chan: ${evenArr}\nday le: ${oddArr}`);
 
-function rmDuplicateVal(Arr){
+console.log("_______c_________");
+function rmDuplicateVal(Arr)
+{
     let len = Arr.length;
     Arr.sort();
-    for(let i = 0; i < n-1; )
+    for(let i = 0; i < len - 1; )
     {
         if(Arr[i] == Arr[i+1])
-            Arr.splice(i+1, 1);
+         {
+            arr.splice(i + 1, 1);
+            --len;
+            }
         else
             ++i;
     }
 }
 rmDuplicateVal(arr)
 console.log(arr);
-// 3 3 3 3 1
