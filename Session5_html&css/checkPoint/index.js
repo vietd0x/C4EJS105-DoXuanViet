463e2d02338de3a409bc61e14920c23ed6f2a888
Viet Do <viet.doxv@gmail.com>
2020-12-30 10:56:17 +0700

    upload hwork

---
+++ Session5_html&css/checkPoint/index.js
@@ -0,0 +1,43 @@
+console.log("1.1 creat a random num from 0 to 1")
+console.log(Math.random().toFixed(2));
+
+console.log("________1.2_______");
+let arr = [2, 5, 6, 9, 10];
+
+console.log(arr[Math.floor(Math.random() * 5)]);
+
+console.log("________1.3_______");
+let quizzes = [
+    {
+        question:'',
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        rightChoice: "",
+    },
+    {
+        question:'',
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        rightChoice: "",
+    },
+    {
+        question:'',
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        rightChoice: "",
+    },
+    {
+        question:'',
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        rightChoice: "",
+    },
+]
