0a1ab192cd2cab22f8ffad14ea119fc914379203
Viet Do <viet.doxv@gmail.com>
2021-01-09 15:25:48 +0700

    first commit

---
+++ Session7_DOM&function/Hwork/index.js
@@ -0,0 +1,59 @@
+// console.log('____2____');
+// setInterval(() => {
+//     console.log('Heehaw');
+// }, 1000);
+
+// console.log('____4____');
+// let li_tag = document.getElementsByTagName('LI');
+// console.log(li_tag[1]);
+// const nav = () =>{
+//     for(let i = 0; i < li_tag.length; i++)
+//         console.log(li_tag[i])
+// }
+// nav();
+
+// console.log('____5____');
+// let singer_name = document.getElementsByTagName('DIV');
+// console.log(singer_name[2]);
+// function allName(){
+//     for(let i = 1; i < singer_name.length - 1; i++)
+//         console.log(singer_name[i]);
+// }
+// allName();
+
+// console.log('____7____');
+// document.getElementById('button1').addEventListener("click", (e) => {
+//     console.log(e.target);
+// }, false);
+// document.getElementById('button2').addEventListener("click", (e) => {
+//     console.log(e.target);
+// });
+
+// document.querySelector('input').addEventListener('keydown', (e) =>{
+//     inp.textContent += ` ${e.key}`;
+// });
+
+// //_______________8_________________
+// function wish(name, thing){
+//     alert(`My name is ${name}, I wish ${thing} this year`);
+// }
+// wish('Viet', 'zZzZ');
+
+// //_______________9_________________
+// let user = prompt("user's name");
+// let userWish = prompt("user's wish");
+// function wish(name, thing){
+//     alert(`My name is ${name}, I wish ${thing} this year`);
+// }
+// wish(user, userWish);
+
+// //_______________10_________________
+// let user = prompt("user's name");
+// let userWish = prompt("user's wish");
+// function wish(name, thing){
+//     if(thing)
+//         alert(`My name is ${name}, I wish ${thing} this year`);
+//     else
+//         alert(`My name is ${name}, I wish zZzZ this year`);
+// }
+// wish(user, userWish);
